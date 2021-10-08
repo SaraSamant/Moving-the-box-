a41661eef5fee35ef1bd471b7086b7e6c4182b79
SaraSamant <92162025+SaraSamant@users.noreply.github.com>
2021-10-08 17:47:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200,200,50,70)
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown (RIGHT_ARROW)) {
+    box.x = box.x + 5 
+  }
+drawSprites()
+}
+
+
+
+
